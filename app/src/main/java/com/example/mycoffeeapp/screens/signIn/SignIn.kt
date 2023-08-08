9e494e0694cd05f3ff046c80624183283b679c45
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-08-08 09:54:30 +0100

    Finished sign in Button design

--- app/src/main/java/com/example/mycoffeeapp/screens/signIn/SignIn.kt
+++ app/src/main/java/com/example/mycoffeeapp/screens/signIn/SignIn.kt
@@ -1,6 +1,7 @@
 package com.example.mycoffeeapp.screens.signIn
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.interaction.collectIsPressedAsState
@@ -8,14 +9,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -27,15 +37,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.room.util.newStringBuilder
 import com.example.mycoffeeapp.ui.theme.MainText
 import com.example.mycoffeeapp.R
+import com.example.mycoffeeapp.naviagtion.Routes
+import com.example.mycoffeeapp.ui.theme.PrimaryColor
 
 
 @Composable
@@ -47,21 +64,72 @@ fun SignIn(navController: NavController){
             .background(Color.White)) {
             Text(
                 text = "Sign In",
-                fontSize = 32.sp,
+                fontSize = 28.sp,
                 color = MainText,
+                fontFamily = FontFamily(Font(R.font.poppins_regular)),
                 modifier = Modifier.padding(top=45.dp)
             )
             Text(
                 text ="Welcome Back",
-                fontSize = 22.sp,
+                fontSize = 18.sp,
                 color = Color.Gray,
                 fontWeight = FontWeight.Light,
-                modifier = Modifier.padding(top=25.dp)
+                modifier = Modifier.padding(top=25.dp,bottom = 50.dp)
             )
 
-            MyTextField("Email",
+            MyTextField("Email address",
                 keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Email),
                 icon= R.drawable.message )
+            MyTextField("Password",
+                keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Password),
+                icon= R.drawable.lock )
+            Text(
+                text ="Forgot Password?",
+                fontSize = 14.sp,
+                color = MainText,
+                textDecoration = TextDecoration.Underline,
+                fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                modifier = Modifier
+                    .padding(top = 25.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(140.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = PrimaryColor,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .size(64.dp)
+                    .align(Alignment.End)
+            ) {
+                Icon(painter = painterResource(id = R.drawable.arrow___right), contentDescription = "next", modifier = Modifier.fillMaxSize())
+            }
+            Spacer(modifier = Modifier.height(140.dp))
+
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(
+                        color = Color.LightGray,
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_regular))
+                    )){
+                        append("New member? ")
+                    }
+                    withStyle(style = SpanStyle(
+                        color = MainText,
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_regular))
+                    )){
+                        append("Sign up")
+                    }
+                } ,
+                modifier = Modifier.clickable {
+                    navController.navigate(Routes.SignUpScreen.name)
+                }
+            )
         }
     }
 }
@@ -96,11 +164,12 @@ fun MyTextField(
             Icon(
                 painter = painterResource(id = icon),
                 contentDescription = "icon",
+                tint= MainText,
                 modifier = Modifier.padding(end=10.dp))
             Box(modifier = Modifier
                 .width(1.dp)
                 .height(20.dp)
-                .background(color = Color.LightGray))
+                .background(color = if (selected) MainText else Color.LightGray))
             TextField(
                 value = text,
                 onValueChange ={
