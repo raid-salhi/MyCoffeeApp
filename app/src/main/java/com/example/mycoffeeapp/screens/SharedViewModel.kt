3a57e290a45825b620fdaf41ae6dd8474e2e6798
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-08-12 15:21:12 +0100

    updated shared viewmodel

--- app/src/main/java/com/example/mycoffeeapp/screens/SharedViewModel.kt
+++ app/src/main/java/com/example/mycoffeeapp/screens/SharedViewModel.kt
@@ -5,10 +5,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.mycoffeeapp.model.Coffee
+import com.example.mycoffeeapp.model.Order
 
 class SharedViewModel: ViewModel() {
     var coffee by mutableStateOf<Coffee?>(null)
+    var order by mutableStateOf<Order?>(null)
     fun sendCoffee(newCoffee: Coffee){
         coffee=newCoffee
+    }
+    fun sendOrder(newOrder: Order){
+        order=newOrder
     }
 }
